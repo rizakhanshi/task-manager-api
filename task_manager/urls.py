a496fd56b48973f66a1f19307bf891feec434c16
Abdirizack Hanshi <hasawi009@gmail.com>
2025-10-15 22:05:45 +0300

    Add token and JWT auth endpoints

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    # DRF token auth
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # JWT auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
